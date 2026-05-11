350b167af172c658a4dcab681b7bf657db8db8c6
VictorSarov <victor.sarov@rentalmatics.com>
2026-05-11 07:20:48 +0100

    good

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import _db from './_db.js';
+
+const typeDefs = `#graphql 
+    type Game {
+        id: ID!
+        title: String!
+        platforms: [String!]!
+    }
+
+    type Review {
+        id: ID!
+        rating: Int!
+        content: String!
+    }
+
+    type Author {
+        id: ID!
+        name: String!
+        verified: Boolean!
+    }
+
+    type Query {
+        reviews: [Review]
+        games: [Game]
+        authors: [Author]
+    }
+`;
 
 
-// typeDefs
+const resolvers = {
+    Query: {
+        games(){
+            return _db.games;
+        },
+        reviews(){
+            return _db.reviews;
+        },
+        authors(){
+            return _db.authors;
+        }
+    }
+}
 
-
-
-
-
-// setup the server
 
 const server = new ApolloServer({
-
+    typeDefs,
+    resolvers
 });
 
 const { url } = await startStandaloneServer(server, {
@@ -19,3 +54,4 @@ const { url } = await startStandaloneServer(server, {
 });
 
 console.log(`Server ready at port: ` + 4000);
+
