33cde235f6572701cd13341cf7b55bc42487f886
VictorSarov <victor.sarov@rentalmatics.com>
2026-05-20 14:27:00 +0100

    all good

--- index.js
+++ index.js
@@ -1,24 +1,104 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-
-
-
+import db from './db.js';
 
 const typeDefs = `#graphql
     type Query {
+        returnString(string: String): String!
+        user: [String!]
 
+        # whole databases 
+        reviews: [Review!]
+        games: [Game!]
+        authors: [Author!]
+
+        # single item
+        review(id: ID!): [Review!]!
+        author(id: ID!): [Author!]
+    }
+
+    type Review {
+        id: ID!
+        rating: Int!
+        content: String!
+        author_id: ID!
+        game_id: ID!
+    }
+
+    type Game {
+        id: ID!
+        title: String!
+        platforms: [String!]!
+        iteration_console: String
+        game_reviews: [Review!]!
+    }
+
+    type Author {
+        id: ID!
+        name: String!
+        verified: Boolean!
     }
 `;
 
-
+const resolvers = {
+    Query: {
+        returnString: (parent, args) => {
+            console.log('query: returnString');
+            return args.string;
+        },
+        user: () => {
+            console.log('query: user');
+            return ['hello'];
+        },
+        reviews: () => {
+            console.log('query: reviews');
+            return db.reviews;
+        },
+        games: () => {
+            console.log('query: games');
+            return db.games;
+        },
+        authors: () => {
+            console.log('query: authors');
+            return db.authors;
+        },
+        review: (parent, args, context) => {
+            console.log('============================');
+            console.log('query: review');
+            console.log('args: ', args);
+            return db.reviews.filter(review => review.id === args.id);
+        },
+        author: (parent, args, context) => {
+            console.log('============================');
+            console.log('query: author');
+            console.log('args: ', args);
+            return db.authors.filter(author => author.id === args.id);
+        }
+    },
+    Game: {
+        iteration_console: (parent) => {
+            console.log('============================');
+            console.log('iteration_console triggered', parent);
+            return;
+        },
+        game_reviews: (parent, args, context) => {
+            console.log('---------------------------------');
+            console.log('Game: game_reviews');
+            console.log('parent: ', parent);
+            return db.reviews.filter(review => review.game_id === parent.id);
+        }
+    }
+};
 
 const server = new ApolloServer({
-    // typeDefs,
-    // resolverFunctions
+    typeDefs,
+    resolvers
 });
 
 const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
+    listen: { port: 4000 }
 });
+
+console.log(`Server ready at ${url}`);
 
 // String, Boolean, Int, ID, Float
